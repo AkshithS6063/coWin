6324ae06f5c826de34bedc032717d9e1ae7b4876
AkshithS <achu.akshith.s@gmail.com>
2021-05-18 02:14:23 -0400

    used express.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 require('dotenv').config();
+const express =  require('express');
+const app = express();
 
-console.log(`env variable ${process.env.TOKEN}`);
 
+app.get('/',function (req, res) {
+	console.log('request on /');
+	res.send('<h1>hello!</h1>');
+});
+
+app.listen(process.env.PORT, function() {
+	console.log('listening on port 3000');
+});
